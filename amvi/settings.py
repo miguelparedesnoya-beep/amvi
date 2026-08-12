60be3870559c00c2e91b3361a7ed1c1057f43fdb
miguelparedesnoya-beep <miguelparedesnoya@gmail.com>
2026-08-12 17:38:50 -0500

    Fix formatting in settings.py

--- amvi/settings.py
+++ amvi/settings.py
@@ -94,15 +94,12 @@ LANGUAGE_CODE = "es-co"
 TIME_ZONE = "America/Bogota"
 
 USE_I18N = True
-
 USE_TZ = True
 
 STATIC_URL = "/static/"
-
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
 MEDIA_URL = "/media/"
-
 MEDIA_ROOT = BASE_DIR / "media"
 
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
